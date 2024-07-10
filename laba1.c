a768baee8a15cbfd117f5105d4477dce28efd2c9
Tolstosheiv Sviatoslav <sviatoslav.tolstosheev@gmail.com>
2024-07-10 21:33:23 +0300

    Correct code

--- laba1.c
+++ laba1.c
@@ -1,15 +1,14 @@
+//Sviatoslav Tolstosheiev, Specialty KI-23009
+//Task
 #include <stdio.h>
 
 int main() {
-    // Введення трьох значень
     double t1, t2, t3;
-    printf("Введіть три значення:\n");
+    printf("Введіть ТРИ значення:\n");
     scanf("%lf %lf %lf", &t1, &t2, &t3);
 
-    // Обчислення загального часу
     double total_time_for_eat_cupcake = 1.0 / (1.0/t1 + 1.0/t2 + 1.0/t3);
 
-    // Виведення результату з округленням до 2 знаків після коми
     printf("Час, необхідний для з’їдання пирога: %.2f годин\n", total_time_for_eat_cupcake);
 
     return 0;
